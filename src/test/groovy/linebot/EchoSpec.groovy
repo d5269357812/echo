f488fbc4e3049797e5d091bc7e24a2c5f2ea7f16
Tsung <d5269357812@gmail.com>
2021-03-14 20:09:02 +0000

    Initial commit

---
+++ src/test/groovy/linebot/EchoSpec.groovy
@@ -0,0 +1,19 @@
+package linebot
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class EchoSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
